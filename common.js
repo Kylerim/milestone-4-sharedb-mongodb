c1a878cdb8e6b56a2d1f82e02eb54ff314e7ac44
Daekyung Kim <timkim0713@gmail.com>
2022-05-04 10:04:47 -0400

    sharedb -> mongodb vm remote

--- common.js
+++ common.js
@@ -1,7 +1,7 @@
 exports.IS_PRODUCTION_MODE = true;
 //exports.PROD_IP = "localhost";
 exports.PROD_IP = "127.0.0.1";
-exports.MONGO_IP = "209.151.155.182";
+exports.MONGO_IP = "194.113.75.125";
 // exports.PROD_IP = "kylerim.cse356.compas.cs.stonybrook.edu";
 exports.LOCAL_IP = "localhost";
 exports.GROUP_ID = "61f9cf4e4261123151824fdc";
